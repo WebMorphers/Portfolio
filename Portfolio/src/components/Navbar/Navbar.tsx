e1a30a48ad7caaef1e4980acb8935eeeb92e0850
othsoh <sohabsouri@gmail.com>
2023-09-23 17:40:26 +0100

    second commit

--- Portfolio/src/components/Navbar/Navbar.tsx
+++ Portfolio/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import './Navbar.css'
 import { Link } from 'react-scroll';
+import img from '../../assets/1427745.png'
 
 
 const Navbar = () => {
@@ -7,10 +8,16 @@ const Navbar = () => {
 
     <div className='container'>
 
-      <img src="" alt="" />
-      <Link href="/"   to=''> About Us</Link>
-      <h1>PROJECTS</h1>
-    </div>
+      <img className='logo' src={img} alt="" />
+      <div className='links'> 
+        <Link href="/"  className='navlinkitem' to=''> About Us</Link>
+        <Link href="/" className='navlinkitem'  to=''> OUR PROJECTS</Link> 
+      </div>
+      <button className="contactButton"> Contact
+  <div className="iconButton">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"></path></svg>
+  </div>
+</button>    </div>
   )
 }
 
