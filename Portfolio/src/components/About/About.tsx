141bb257789a6d2723775c8162369b689fa7bca2
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-09-25 18:50:37 +0000

    9

--- Portfolio/src/components/About/About.tsx
+++ Portfolio/src/components/About/About.tsx
@@ -1,8 +1,20 @@
 import "./about.css"
+import logo from '../../assets/nobg_logo.png'
 
 const About = () => {
   return (
-    <div>About Web Morphers</div>
+    <div className="container">
+      <div><h1>About Web Morphers</h1>
+        <p className="pp">We are Web Morphers, a passionate team of front-end developers specializing in ReactJS.
+         We craft visually stunning and highly interactive web experiences,
+          shaping the future of the web one project at a time. 
+          Join us on our journey to transform the digital landscape.</p>
+      </div>
+ 
+     
+    <div><img src={logo} alt="" /></div>
+    
+    </div>
   )
 }
 
