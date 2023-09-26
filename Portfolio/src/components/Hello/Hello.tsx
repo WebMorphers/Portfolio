4424f60efb00144209837a411ea6c17e3e8adb43
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-09-26 14:56:31 +0000

    6

--- Portfolio/src/components/Hello/Hello.tsx
+++ Portfolio/src/components/Hello/Hello.tsx
@@ -3,18 +3,12 @@ import './hello.css'
 const Hello = () => {
   return (
     
-    <div className='container'>
-      
+    <div className='container'> 
       <div className='text'> 
         <p className='description'>
         Hello We Are<br />  <span className="span" >WebMorphers</span><br /> A Team Of Web Developers   
         </p>
-      </div>
-      {/* <div className='imageContainer'>
-        <img className='image' src={image} alt="" />
-
-      </div> */}
-      
+      </div> 
     </div>
   )
 }
