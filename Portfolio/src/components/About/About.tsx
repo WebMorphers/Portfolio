10f434dcbbb242de53cad795a2aff91f5e22123a
othsoh <sohabsouri@gmail.com>
2023-09-27 15:33:56 +0100

    ay

--- Portfolio/src/components/About/About.tsx
+++ Portfolio/src/components/About/About.tsx
@@ -4,17 +4,17 @@ import logo from '../../assets/nobg_logo.png'
 const About = () => {
   return (
     <div className="containerr">
-      <div className="h1p"><h1>About Web Morphers</h1>
+      <div className="h1p"><h1 className="titre">About Web Morphers</h1>
       <br />
-      <div>
-  <p className="pp">
-    <span className="highlight">Web Morphers</span>, a budding team of <span className="highlight">ReactJS experts</span>, is redefining web development. Despite our newcomer status, we're driven by <span className="highlight">passion</span> and <span className="highlight">determination</span>. Our mission: to create captivating, interactive web experiences that shape the digital landscape.
-  </p>
-  <br />
-  <p>
-    We embrace <span className="highlight">innovation</span>, constantly evolving to stay at the forefront of <span className="highlight">technology trends</span>. Our <span className="highlight">agility</span> and <span className="highlight">adaptability</span> enable us to exceed expectations and turn ambitious concepts into reality. Join us on this dynamic journey, as we reshape the future of the web.
-  </p>
-</div>
+      <div className="text">
+      <p className="pp">
+        <span className="highlight">Web Morphers</span>, a budding team of <span className="highlight">ReactJS experts</span>, is redefining web development. Despite our newcomer status, we're driven by <span className="highlight">passion</span> and <span className="highlight">determination</span>. Our mission: to create captivating, interactive web experiences that shape the digital landscape.
+      </p>
+      <br />
+      <p className="pp">
+        We embrace <span className="highlight">innovation</span>, constantly evolving to stay at the forefront of <span className="highlight">technology trends</span>. Our <span className="highlight">agility</span> and <span className="highlight">adaptability</span> enable us to exceed expectations and turn ambitious concepts into reality. Join us on this dynamic journey, as we reshape the future of the web.
+      </p>
+    </div>
 
       </div>
  
