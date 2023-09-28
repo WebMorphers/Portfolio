f5edd6641f59df88cdb710b96397393f933e5671
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-09-28 19:08:42 +0000

    d

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -5,36 +5,34 @@ import Footer from "./components/Footer/Footer";
 import About from "./components/About/About";
 import Projects from "./components/projects/Projects";
 import Links from "./components/Links/Links";
-import './app.css';
-
+import "./app.css";
 
 function App() {
   return (
     <>
-    <Navbar />
-    <div className="app-container"> 
-    
-      <div id="hello" className="hello-container">
-        <Hello />
-        <br />
-        <br /><br />
-        <Links />   
-        <span className="scroll-icon">
-      <span className="scroll-icon__dot"></span> 
-    </span>  
-      </div>     
-    </div>
-    
+      <Navbar />
+      <div className="app-container">
+        <div id="hello" className="hello-container">
+          <Hello />
+          <br />
+          <br />
+          <br />
+          <Links />
+          <span className="scroll-icon">
+            <span className="scroll-icon__dot"></span>
+          </span>
+        </div>
+      </div>
 
-    <div id="about_us" className="about-container">
-      <About />
-    </div> 
-    <div id="projects" className="projects_container">
-      <Projects />
-    </div> 
-    <div id="contact" className="contact-container">
-      <Contact />
-    </div> 
+      <div id="about_us" className="about-container">
+        <About />
+      </div>
+      <div id="projects" className="projects_container">
+        <Projects />
+      </div>
+      <div id="contact" className="contact-container">
+        <Contact />
+      </div>
 
       <Footer />
     </>
