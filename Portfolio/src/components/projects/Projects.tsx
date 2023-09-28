46486fc351cb501a9a8f202d711ffc40fda273da
othsoh <sohabsouri@gmail.com>
2023-09-28 12:37:51 +0100

    ayay

--- Portfolio/src/components/projects/Projects.tsx
+++ Portfolio/src/components/projects/Projects.tsx
@@ -3,7 +3,12 @@ import './Projects.css'
 const Projects = () => {
   return (
     <div  className='ontainer'>
-        Projects
+      <h1 className="pconty">Projects</h1>
+      <div className='content'>
+      <p className="pcont">
+        Avec une expérience riche dans le développement web, nous sommes une équipe de créateurs passionnés par l'innovation et l'excellence technique. Notre parcours diversifié comprend des collaborations fructueuses avec une gamme variée de clients, des startups aux grandes entreprises. Nous nous engageons à créer des sites web sur mesure qui allient esthétique, fonctionnalité et performance. Notre expertise technique de pointe nous permet d'adopter les dernières technologies pour répondre aux besoins uniques de chaque client. Nous sommes fiers de repousser constamment les limites de la créativité pour offrir des expériences en ligne exceptionnelles.
+        </p>
+      </div>
     </div>
   )
 }
