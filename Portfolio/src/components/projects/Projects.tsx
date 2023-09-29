e605adf21552242fe240d40643e29d2c0abf7845
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-09-29 19:58:13 +0000

    Update Projects.tsx

--- Portfolio/src/components/projects/Projects.tsx
+++ Portfolio/src/components/projects/Projects.tsx
@@ -1,6 +1,5 @@
 import './Projects.css'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+ 
 
 const Projects = () => {
   return (
@@ -18,17 +17,6 @@ const Projects = () => {
       of creativity to deliver exceptional online experiences.  
       </p>
       </div>
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
     </div>
   )
 }
