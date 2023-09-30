272ba4755377613c5e781f805a25a83f5f3a8082
othsoh <sohabsouri@gmail.com>
2023-10-01 00:09:20 +0100

    t

--- Portfolio/tailwind.config.js
+++ Portfolio/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
