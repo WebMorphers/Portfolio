6b9d6a8f91be070c1a64f9434eb09995064ac088
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-01 22:10:32 +0000

    d

--- Portfolio/src/components/Hello/Hello.tsx
+++ Portfolio/src/components/Hello/Hello.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 
 const Hello = () => {
   return (
-    <div className='bg-gray-100 min-h-screen flex justify-center items-cente md:max-w-50 pt-20   '>
+    <div className='bg-gray-100 min-h-screen flex justify-center   pt-20 '>
       <div className='text-center'>
-        <p className='text-5xl font-bold mb-4'>
-          
+        <p className='sm:text-[25px] md:text-[60px] lg:text-[60px]  font-bold mb-4'> 
           Hello We Are<br />
-          <span className="text-yellow-500 font-serif text-8xl uppercase animate-pulse mt-5">
+          <span className="text-yellow-500 font-serif sm:text-[32px] md:text-[70px] lg:text-[130px] uppercase animate-pulse mt-5">
             WebMorphers
           </span><br />
           A Team Of Web Developers
--- Portfolio/tailwind.config.js
+++ Portfolio/tailwind.config.js
@@ -4,6 +4,22 @@ export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}',"./index.html"],
   theme: {
     extend: {},
+    screens: {
+      'sm': '350px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '862px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1170px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [
     require('flowbite/plugin')
