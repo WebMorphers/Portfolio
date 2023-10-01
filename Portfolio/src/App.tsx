dd8af77ab5e5098fbca154d8570a34aaf0453b5a
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-01 18:48:29 +0000

    4

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -11,6 +11,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Hello />
       <Footer />
     </>
   );
--- Portfolio/src/components/Hello/Hello.tsx
+++ Portfolio/src/components/Hello/Hello.tsx
@@ -1,16 +1,20 @@
-import './hello.css'
- 
+import React from 'react';
+
 const Hello = () => {
   return (
-    
-    <div className='scontainer'> 
-      <div className='text'> 
-        <p className='description'>
-        Hello We Are<br />  <span className="span" >WebMorphers</span><br /> A Team Of Web Developers   
+    <div className='bg-gray-100 min-h-screen flex justify-center items-cente md:max-w-50 pt-20   '>
+      <div className='text-center'>
+        <p className='text-5xl font-bold mb-4'>
+          
+          Hello We Are<br />
+          <span className="text-yellow-500 font-serif text-8xl uppercase animate-pulse mt-5">
+            WebMorphers
+          </span><br />
+          A Team Of Web Developers
         </p>
-      </div> 
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hello
+export default Hello;
