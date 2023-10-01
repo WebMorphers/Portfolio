a7a3acd8294ada74a42baa95af06e2fd29882b49
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-01 00:33:59 +0000

    1

--- Portfolio/tailwind.config.js
+++ Portfolio/tailwind.config.js
@@ -1,13 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< Updated upstream
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-=======
   content: ['./src/**/*.{js,jsx,ts,tsx}',"./index.html"],
->>>>>>> Stashed changes
   theme: {
     extend: {},
   },
