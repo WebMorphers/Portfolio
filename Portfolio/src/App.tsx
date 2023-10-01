678796b6c81187cbfb2ca0b84411e03860d43244
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-01 17:48:52 +0000

    11

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -10,16 +10,10 @@ import "./app.css";
 function App() {
   return ( 
     <>
-    <nav>
       <Navbar />
-<<<<<<< Updated upstream
-      </nav>
-
-=======
-      
+       
       <Footer />
->>>>>>> Stashed changes
-      </>
+    </>
   );
 }
 
