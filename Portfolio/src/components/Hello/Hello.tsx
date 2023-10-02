ee278d1f889b62822ede3dc6d7708e7f2f74d470
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-02 16:49:27 +0000

    4

--- Portfolio/src/components/Hello/Hello.tsx
+++ Portfolio/src/components/Hello/Hello.tsx
@@ -4,9 +4,9 @@ const Hello = () => {
   return (
     <div className='bg-gradient-to-r from-[#FFD166] to-[#FFFCF9] to-100%  min-h-screen flex justify-center pt-20'>
       <div className='text-center mx-auto sm:pt-24 lg:pt-32'>
-        <p className='font-bold mb-4 sm:text-5xl lg:text-6xl'> 
+        <p className='font-bold mb-4 sm:text-3xl lg:text-7xl md:text-5xl'> 
           Hello We Are<br />
-          <span className="text-7xl sm:text-6xl text-yellow-500 font-serif uppercase animate-pulse mt-5">
+          <span className=" sm:text-4xl md:text-7xl xl:text-8xl text-yellow-500 font-serif uppercase animate-pulse mt-5">
             WebMorphers
           </span><br />
           A Team Of Web Developers
