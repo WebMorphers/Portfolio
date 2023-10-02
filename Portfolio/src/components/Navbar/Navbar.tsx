35f9c34b7b699c6a311684df03c084ba011f9df3
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-02 16:58:53 +0000

    d

--- Portfolio/src/components/Navbar/Navbar.tsx
+++ Portfolio/src/components/Navbar/Navbar.tsx
@@ -1,13 +1,11 @@
 import "./Navbar.css";
 import logo from "../../assets/nobg_logo.png";
-import  { useEffect ,useState } from 'react'; 
-import { AiOutlineMenu,AiOutlineClose } from 'react-icons/ai';
-
-
+import { useEffect, useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
-  const [nav,setnav]=useState(false); 
-  function chang(){
+  const [nav, setnav] = useState(false);
+  function chang() {
     setnav(!nav);
   }
   const scrollToSection = (sectionId: string) => {
@@ -20,11 +18,11 @@ const Navbar = () => {
     }
   };
   useEffect(() => {
-     const internalLinks = document.querySelectorAll('a[href^="#"]');
+    const internalLinks = document.querySelectorAll('a[href^="#"]');
     internalLinks.forEach((link) => {
-      link.addEventListener('click', (e) => {
+      link.addEventListener("click", (e) => {
         e.preventDefault();
-        const targetId = link.getAttribute('href')?.slice(1); 
+        const targetId = link.getAttribute("href")?.slice(1);
         if (targetId) {
           scrollToSection(targetId);
         }
@@ -33,60 +31,83 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className='flex w-full  top-0 fixed justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white backdrop-blur-md bg-orange-600 bg-opacity-10	'>
-       <img className=" h-20 w-23" src={logo} alt="" />
-      <ul className='hidden md:flex'>
-        <li className='p-4'><a
-          className="no-underline mx-4 cursor-pointer font-light	 text-gray-900 hover:text-orange-600"
-          onClick={() => scrollToSection("about_us")}
-          href="#about_us"
-        >
-          ABOUT US
-        </a></li>
-        <li className='p-4'><a
-          className="no-underline mx-4 cursor-pointer font-thin text-gray-900 hover:text-orange-600"
-          onClick={() => scrollToSection("projects")}
-          href="#projects">
-          OUR PROJECTS
-        </a></li>
-
-         
-      </ul>
-      <ul className='hidden md:flex'>
-      <li className='p-4'> <a
-          className="no-underline mx-4 cursor-pointer font-normal	text-gray-900 hover:text-orange-600"
-          onClick={() => scrollToSection("contact")}
-          href="#contact"
-        >
-          CONTACT US
-        </a></li>
-      </ul>
-      <div onClick={chang} className='block md:hidden'>
-          {nav ? <AiOutlineClose size={20} color={"black"} /> : <AiOutlineMenu size={20} color={"black"} />}
-      </div>
-      <ul className={nav ? ' fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-amber-700 ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+    <div className="flex w-full  top-0 fixed justify-between items-center h-24 mx-auto px-4 text-white backdrop-blur-md bg-orange-600 bg-opacity-10	">
       <img className=" h-20 w-23" src={logo} alt="" />
-        <li className='p-4 '><a
-          className="no-underline mx-4 cursor-pointer font-thin text-white hover:text-orange-600"
-          onClick={() => scrollToSection("about_us")}
-          href="#about_us"
-        >
-          ABOUT US
-        </a></li>
-        <li className='p-4'><a
-          className="no-underline mx-4 cursor-pointer font-thin text-white hover:text-orange-600 "
-          onClick={() => scrollToSection("projects")}
-          href="#projects"
-        >
-          OUR PROJECTS
-        </a></li>
-        <li className='p-4'> <a
-          className="no-underline mx-4 cursor-pointer font-thin text-white hover:text-orange-600 "
-          onClick={() => scrollToSection("contact")}
-          href="#contact"
-        >
-          CONTACT US
-        </a></li>
+      <ul className="hidden md:flex">
+        <li className="p-4">
+          <a
+            className="no-underline mx-4 cursor-pointer font-light	 text-gray-900 hover:text-orange-600"
+            onClick={() => scrollToSection("about_us")}
+            href="#about_us"
+          >
+            ABOUT US
+          </a>
+        </li>
+        <li className="p-4">
+          <a
+            className="no-underline mx-4 cursor-pointer font-thin text-gray-900 hover:text-orange-600"
+            onClick={() => scrollToSection("projects")}
+            href="#projects"
+          >
+            OUR PROJECTS
+          </a>
+        </li>
+      </ul>
+      <ul className="hidden md:flex">
+        <li className="p-4">
+          {" "}
+          <a
+            className="no-underline mx-4 cursor-pointer font-normal	text-gray-900 hover:text-orange-600"
+            onClick={() => scrollToSection("contact")}
+            href="#contact"
+          >
+            CONTACT US
+          </a>
+        </li>
+      </ul>
+      <div onClick={chang} className="block md:hidden">
+        {nav ? (
+          <AiOutlineClose size={20} color={"black"} />
+        ) : (
+          <AiOutlineMenu size={20} color={"black"} />
+        )}
+      </div>
+      <ul
+        className={
+          nav
+            ? " fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-amber-700 ease-in-out duration-500"
+            : "ease-in-out duration-500 fixed left-[-100%]"
+        }
+      >
+        <img className=" h-20 w-23" src={logo} alt="" />
+        <li className="p-4 ">
+          <a
+            className="no-underline mx-4 cursor-pointer font-thin text-white hover:text-orange-600"
+            onClick={() => scrollToSection("about_us")}
+            href="#about_us"
+          >
+            ABOUT US
+          </a>
+        </li>
+        <li className="p-4">
+          <a
+            className="no-underline mx-4 cursor-pointer font-thin text-white hover:text-orange-600 "
+            onClick={() => scrollToSection("projects")}
+            href="#projects"
+          >
+            OUR PROJECTS
+          </a>
+        </li>
+        <li className="p-4">
+          {" "}
+          <a
+            className="no-underline mx-4 cursor-pointer font-thin text-white hover:text-orange-600 "
+            onClick={() => scrollToSection("contact")}
+            href="#contact"
+          >
+            CONTACT US
+          </a>
+        </li>
       </ul>
     </div>
   );
