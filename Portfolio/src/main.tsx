8f6efa62a0cc251c0ae84587494b87832afab8c1
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-08 21:24:00 +0000

    Update main.tsx

--- Portfolio/src/main.tsx
+++ Portfolio/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { inject } from '@vercel/analytics';
+import { inject } from '@vercel/analytics'; 
  
 
 inject();
