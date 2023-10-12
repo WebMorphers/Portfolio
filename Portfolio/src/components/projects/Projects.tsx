655b134b9cdd5ca8bcebc329adb924535c734988
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-12 22:17:26 +0000

    f

--- Portfolio/src/components/projects/Projects.tsx
+++ Portfolio/src/components/projects/Projects.tsx
@@ -1,35 +1,36 @@
- 
 import "./Projects.css";
-import Nexcent from '../../assets/nexcent.png'
-import Notapark from '../../assets/notapark.png'
+import Nexcent from "../../assets/nexcent.png";
+import Notapark from "../../assets/notapark.png";
 
-
-const projects=[{
-  img:Nexcent,
-  title: "Nexcent",
-  subtitle:"Responsive Landing Page",
-  description:"Responsive Landing Page Design | Website Home Page Design | Agency Website UI Design",
-  link:"https://nexcent-pi.vercel.app/",
-},
-{
-  img:Notapark,
-  title: "Notapark",
-  subtitle:"Hoodies Landing Page",
-  description:"Responsive Landing Page Design | Website Home Page Design | Agency Website UI Design",
-  link:"https://notapark.vercel.app/",
-},]
-
+const projects = [
+  {
+    img: Nexcent,
+    title: "Nexcent",
+    subtitle: "Responsive Landing Page",
+    description:
+      "Responsive Landing Page Design | Website Home Page Design | Agency Website UI Design",
+    link: "https://nexcent-pi.vercel.app/",
+  },
+  {
+    img: Notapark,
+    title: "Notapark",
+    subtitle: "Hoodies Landing Page",
+    description:
+      "Responsive Landing Page Design | Website Home Page Design | Agency Website UI Design",
+    link: "https://notapark.vercel.app/",
+  },
+];
 
 const Projects = () => {
-
-
   return (
     <div className="lg:py-[10%] bg-gradient-to-b from-amber-300 via-amber-400 to-yellow-400 container mx-auto pt-20 flex items-center justify-center flex-col min-w-full">
       <h1 className="xs:text-[50px] sm:text-[60px] md:text-[66px]">Projects</h1>
-      <div className="mt-4 line-height-18 word-spacing-4px px-[12%] align-center justify-center text-center" id="te">
-        
+      <div
+        className="mt-4 line-height-18 word-spacing-4px px-[12%] align-center justify-center text-center"
+        id="te"
+      >
         <p className="xs:text-[18px] sm:text-[20px] md:text-[20px]">
-        With a wealth of experience in web development, we are a team of
+          With a wealth of experience in web development, we are a team of
           dedicated creators driven by innovation and technical excellence. Our
           diverse portfolio includes successful collaborations with a wide range
           of clients, from startups to large enterprises. We are committed to
@@ -42,7 +43,7 @@ const Projects = () => {
       </div>
       <div>
         <div className="grid md:grid-cols-2 xl:gap-20 gap-20 md:gap-10 md:m-16 p-8 lg:m-24 m-8 ">
-          {projects.map(project =>(
+          {projects.map((project) => (
             <div className="flex flex-col px-4 md:px-10 py-3 rounded-xl shadow-2xl items-center gap-5 hover:bg-yellow-400 cursor-pointer">
               <div>
                 <img src={project.img} alt="" />
@@ -51,7 +52,13 @@ const Projects = () => {
                 <h1 className="text-2xl font-semibold">{project.title}</h1>
                 <h2 className="text-xl ">{project.subtitle}</h2>
                 <p className="text-sm opacity-80 ">{project.description}</p>
-                <a className="px-2 py-3 bg-[#EE9322] rounded w-max hover:bg-[#D83F31] " href={project.link}>Live preview</a>
+                <a
+                  className="px-2 py-3 bg-[#EE9322] rounded w-max hover:bg-[#D83F31] "
+                  href={project.link}
+                  target="-blank"
+                >
+                  Live preview
+                </a>
               </div>
             </div>
           ))}
